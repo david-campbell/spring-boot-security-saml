944d47e5dfd32d6c3bd3325e9222f66d4ee721d5
David Campbell <David.Campbell@ontario.ca>
2017-04-25 09:14:47 -0400

    updated to use cf instead of bluemix. updated to logout and then login before push

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,12 @@
 cd \
-cd C:\Users\CampbellD\git\spring-boot-security-saml
+cd C:\_BlueMix\git\spring-boot-security-saml
 call mvn clean
 call mvn package -U
+cf logout
+cf login -u David.campbell@ontario.ca -p Meaghan1!!** -a https://api.ops1.ca-east.bluemix.net -o OPS-CAC-Dev1 -s "CAC-POC Functional Tests"
 cd target
 call cf push DCJavaSpringSaml2Sample -p spring-boot-security-saml-sample-1.4.0.RELEASE.war
-IF "%1"=="logs" (call bluemix app logs DCJavaSpringSaml2Sample) 
+IF "%1"=="logs" (call cf logs DCJavaSpringSaml2Sample) 
 cd ..
 
 
